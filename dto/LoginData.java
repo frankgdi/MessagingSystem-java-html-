19f48e73d8939891bc9978a6e1b63938cba1338c
frankgdi <frankworking0412@gmail.com>
2026-05-30 17:24:36 +0800

    Update LoginData.java

--- dto/LoginData.java
+++ dto/LoginData.java
@@ -1,1 +1,7 @@
+package com.example.messaging.dto;
 
+public class LoginData {
+    private String name;
+    public String getName() { return name; }
+    public void setName(String name) { this.name = name; }
+}
