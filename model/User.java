11b3c4baee8f253b8a96318edf5177620b1a3701
frankgdi <frankworking0412@gmail.com>
2026-05-30 17:23:12 +0800

    Update User.java

--- model/User.java
+++ model/User.java
@@ -1,4 +1,4 @@
-package model;
+package com.example.messaging.model;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -16,11 +16,12 @@ public class User {
 
     public void addFriend(int friendId) {
         if (!this.friends.contains(friendId)) {
+            this.friends.append(friendId); // 在 Java 中建議寫成封裝方法
             this.friends.add(friendId);
         }
     }
 
-    // Getters
+    // Getters and Setters (Java 需要這些來轉換為 JSON)
     public int getUserId() { return userId; }
     public String getName() { return name; }
     public List<Integer> getFriends() { return friends; }
