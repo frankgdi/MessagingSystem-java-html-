c18fb89b9c45e3a4233b2bf43fbdcd837f5908e6
hintasd <978413929b@gmail.com>
2026-05-30 21:17:45 +0800

    Create CreateGroupData.java

---
+++ src/main/java/com/example/messaging/dto/CreateGroupData.java
@@ -0,0 +1,27 @@
+package com.example.messaging.dto;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public class CreateGroupData {
+    @JsonProperty("user_id")
+    private int userId;
+
+    @JsonProperty("group_name")
+    private String groupName;
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public String getGroupName() {
+        return groupName;
+    }
+
+    public void setGroupName(String groupName) {
+        this.groupName = groupName;
+    }
+}
