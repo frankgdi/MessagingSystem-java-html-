ccebe7ba29c0cf4001bb0341a2dc7d716d8f4840
frankgdi <frankworking0412@gmail.com>
2026-06-01 03:02:28 +0800

    Update AuthService.java

--- src/main/java/com/example/messaging/service/AuthService.java
+++ src/main/java/com/example/messaging/service/AuthService.java
@@ -1,1 +1,24 @@
+package com.example.messaging.service;
 
+import com.example.messaging.model.User;
+import com.example.messaging.repository.UserDB;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AuthService {
+
+    @Autowired
+    private UserDB userDb;
+
+    public boolean verifyCredentials(String username, String password) {
+        User user = userDb.findUserByUsername(username);
+        if (user == null) return false;
+        
+        return user.getPassword().equals(password);
+    }
+
+    public User getUserByUsername(String username) {
+        return userDb.findUserByUsername(username);
+    }
+}
